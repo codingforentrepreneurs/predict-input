f1c8eb5a52063e62cc364ed725f7b067836a3147
CodingForEntrepreneurs <hello@teamcfe.com>
2023-02-06 16:55:03 -0600

    12 - predict-input and Form Submit

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,21 @@
 <template>
-    <form>
-        <predict-input name="pred" pred-url="/my/api/endpoint/"></predict-input>
+    <form ref="formRef" @submit="handleSubmit">
+        <predict-input name="content" pred-url="/my/api/endpoint/"></predict-input>
+
+        <button type="submit">Send</button>
     </form>
 </template>
 
 
 <script setup>
+import {ref} from 'vue'
 import PredictInput from './PredictInput.vue';
 
+const formRef = ref(null)
+const handleSubmit = event => {
+    event.preventDefault()
+    const formData = new FormData(formRef.value)
+    const jsonData = Object.fromEntries(formData)
+    console.log(jsonData)
+}
 </script>
