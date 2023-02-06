29bd6ebee06dd68456a47a007067e7ad508339ee
CodingForEntrepreneurs <hello@teamcfe.com>
2023-02-06 10:19:20 -0600

    3 - Textarea Echo

---
+++ src/components/PredictInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <textarea 
+        v-model="userDataModel" 
+        @keyup="handleKeyUp"
+        placeholder="Your input.."
+        ></textarea>
+    <textarea v-model="predDataModel" readonly></textarea>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const userDataModel = ref("")
+const predDataModel = ref("")
+
+const handleKeyUp = (event) => {
+    // predDataModel.value = event.target.value
+    predDataModel.value = userDataModel.value
+}
+
+</script>
