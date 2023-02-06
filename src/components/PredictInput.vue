74d4cb250aaadac8464592a2c2ece3ed8e22b57d
CodingForEntrepreneurs <hello@teamcfe.com>
2023-02-06 10:38:18 -0600

    5 - Dynamic Textarea Size & Layering

--- src/components/PredictInput.vue
+++ src/components/PredictInput.vue
@@ -1,22 +1,39 @@
 <template>
     <textarea 
+        ref="foregroundInput"
         v-model="userDataModel" 
         @keyup="handleKeyUp"
         placeholder="Your input.."
         class="input-top"
         ></textarea>
-    <textarea v-model="predDataModel"  class="input-bottom" readonly></textarea>
+    <textarea ref="backgroundInput" v-model="predDataModel"  class="input-bottom" readonly></textarea>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 
+const foregroundInput = ref(null)
+const backgroundInput = ref(null)
 const userDataModel = ref("")
 const predDataModel = ref("")
 
+onMounted(()=>{
+    styleMatchBackground()
+})
+
+const styleMatchBackground = () => {
+    backgroundInput.value.style.position = "absolute";
+    backgroundInput.value.style.borderColor = "transparent";
+    // backgroundInput.value.style.color = "transparent";
+    backgroundInput.value.style.top = foregroundInput.value.offsetTop + "px";
+    backgroundInput.value.style.left = foregroundInput.value.offsetLeft + "px";
+    backgroundInput.value.style.width = foregroundInput.value.offsetWidth + "px";
+    backgroundInput.value.style.height = foregroundInput.value.offsetHeight + "px";
+}
 const handleKeyUp = (event) => {
     // predDataModel.value = event.target.value
     predDataModel.value = userDataModel.value
+    styleMatchBackground()
 }
 
 </script>
@@ -29,7 +46,6 @@ const handleKeyUp = (event) => {
     background: transparent;
     width: 100%;
     z-index: 2;
-    resize: none;
 }
 .input-bottom {
     color: gray;
@@ -37,7 +53,8 @@ const handleKeyUp = (event) => {
     border-color: transparent;
     background: transparent;
     width: 100%;
-    z-index: 2;
+    z-index: 1;
+    resize: none;
 }
 
 </style>
